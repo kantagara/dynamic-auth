bb72c6ae28a01b427d8501c2693f5c2f98b415e6
Tam Vo Minh <tam.vominh@gameloft.com>
2025-08-20 11:54:01 +0700

    Revert "disable (setting will be handled other way)"
    This reverts commit 26677e7dddb57bc9a7cec33789304fca2e9168c2.

--- Assets/Editor/MobileBuildSettings.cs
+++ Assets/Editor/MobileBuildSettings.cs
@@ -7,20 +7,20 @@ public class MobileBuildSettings
     public static void SetupMobileBuild()
     {
         // iOS Settings
-        // PlayerSettings.iOS.targetOSVersionString = "12.0";
-        // PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.iOS, "com.yourcompany.dynamicunity");
+        PlayerSettings.iOS.targetOSVersionString = "12.0";
+        PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.iOS, "com.yourcompany.dynamicunity");
         
         // Android Settings  
-        // PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel23; // Android 6.0
-        // PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevel33; // Android 13
-        // PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.yourcompany.dynamicunity");
+        PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel23; // Android 6.0
+        PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevel33; // Android 13
+        PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.yourcompany.dynamicunity");
         
         // Internet permissions
-        // PlayerSettings.Android.forceInternetPermission = true;
+        PlayerSettings.Android.forceInternetPermission = true;
         
-        // Debug.Log("[MobileBuildSettings] Mobile build settings configured");
-        // Debug.Log("iOS Bundle ID: com.yourcompany.dynamicunity");
-        // Debug.Log("Android Package: com.yourcompany.dynamicunity");
-        // Debug.Log("Deeplink scheme: dynamicunity://");
+        Debug.Log("[MobileBuildSettings] Mobile build settings configured");
+        Debug.Log("iOS Bundle ID: com.yourcompany.dynamicunity");
+        Debug.Log("Android Package: com.yourcompany.dynamicunity");
+        Debug.Log("Deeplink scheme: dynamicunity://");
     }
 }
